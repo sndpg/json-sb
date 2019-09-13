f5e66ee9fbf1a3ccc18f603563cc6e9317b88089
sykq <genericmailacc@gmail.com>
2019-09-13 18:29:39 +0200

    init

---
+++ src/main/java/org/psc/jsonsb/JsonSbApplication.java
@@ -0,0 +1,13 @@
+package org.psc.jsonsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonSbApplication.class, args);
+    }
+
+}
